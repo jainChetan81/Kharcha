8c7230cf61068ed1d4dfc5c527d47380340bf0d5
jainChetanHN <chetan.jain@hackerearth.com>
2026-04-15 23:32:33 +0530

    lint fix

--- lib/db/connection.ts
+++ lib/db/connection.ts
@@ -1,8 +1,8 @@
 import { drizzle, type ExpoSQLiteDatabase } from "drizzle-orm/expo-sqlite";
 import { migrate } from "drizzle-orm/expo-sqlite/migrator";
 import * as SQLite from "expo-sqlite";
-import migrations from "../../drizzle/migrations";
 import { DB_NAME } from "@/lib/constants";
+import migrations from "../../drizzle/migrations";
 
 const expo = SQLite.openDatabaseSync(DB_NAME);
 const db: ExpoSQLiteDatabase = drizzle(expo, { logger: false });
